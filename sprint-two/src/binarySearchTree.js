dfe498c30044d47f5994ae48d4b723bd1faa736c
Emp-Norton <jamesjelenko@gmail.com>
2017-12-02 15:13:51 -0800

    Binary Search Tree passing all tests.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,67 +1,71 @@
 var BinarySearchTree = function(value) {
   var someInstance = Object.create(BinarySearchTree.prototype);
   someInstance.value = value;
-  someInstance.left = undefined;
-  someInstance.right = undefined;
+  someInstance.left = null;
+  someInstance.right = null;
   return someInstance;  
 };
 
 
 BinarySearchTree.prototype.insert = function(value) {
   var newNode = BinarySearchTree(value);
-  console.log(this)
   var current = this;
+
   if (newNode.value < current.value) {
-    if (current.left === undefined) {
-      current.left = newNode;
-    } else {
-      current.left.insert(value);
-    }
+    !current.left ? current.left = newNode : current.left.insert(value);
   }
   if (newNode.value > current.value) {
-    if (current.right === undefined) {
-      current.right = newNode;
-    } else {
-      current.right.insert(value);
-    }
+    !current.right ? current.right = newNode : current.right.insert(value);
   }
-  
+
 };
 
 BinarySearchTree.prototype.contains = function(target) {
   var result; 
-  
   var topNode = this;
 
   var checkNode = function(node) {  
     if (node.value === target) {
-     return true;
-    };
-
+      return true;
+    }
     if (target < node.value) {
       if (node.left) {
-       return checkNode(node.left)
+        return checkNode(node.left);
       } 
     } 
-    if (target > node.value){
+    if (target > node.value) {
       if (node.right) {
-        return checkNode(node.right)
+        return checkNode(node.right);
       } 
     }
-    return false
- };
+    return false;
+  };
  
-    return checkNode(topNode)
- 
+  return checkNode(topNode); 
 };
 
+BinarySearchTree.prototype.depthFirstLog = function (callback) {
+  var usedVals = [];
+  callback(this.value);
 
+  var getVals = function(node) {
+    if (node.left) {
+      getVals(node.left);
+    } else {
+      if (!usedVals.includes(node.value)) {
+        usedVals.push(node.value);
+        callback(node.value);
+      }
+    }
+    if (node.right) {
+      getVals(node.right);
+    } else {
+      if (!usedVals.includes(node.value)) {
+        usedVals.push(node.value);
+        callback(node.value);
+      }
+    }
+  };
 
-
-
-
-
-BinarySearchTree.prototype.depthfirstLog = function (callback) {
-
+  getVals(this);
 };
-
