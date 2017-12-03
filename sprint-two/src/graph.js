89bf062ce932c5ec8a3cd5d5670f34a24ac53526
Emp-Norton <jamesjelenko@gmail.com>
2017-12-02 17:35:46 -0800

    Cleaned up comments / code.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,7 +2,6 @@ var Graph = function() {
   this.nodes = [];
 };
 
-// Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
   var newNode = {};
   newNode.value = node;
@@ -11,7 +10,6 @@ Graph.prototype.addNode = function(node) {
   this.nodes.push(newNode);
 };
 
-// Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
   var found = false;
   this.nodes.forEach(function(n) { 
@@ -22,7 +20,6 @@ Graph.prototype.contains = function(node) {
   return found;
 };
 
-// Removes a node from the graph.
 Graph.prototype.removeNode = function(nodeVal) {
   var targetIndex;
   this.nodes.forEach(function(node, index) {
@@ -39,7 +36,6 @@ Graph.prototype.removeNode = function(nodeVal) {
   this.nodes.splice(targetIndex, 1);
 };
 
-// Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   var fromNodeIndex = this.findIndexesByValues(fromNode, toNode)[0];
   var toNodeIndex = this.findIndexesByValues(fromNode, toNode)[1];
@@ -51,7 +47,6 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
   return this.nodes[fromNodeIndex].edges.includes(toNode) || this.nodes[toNodeIndex].edges.includes(fromNode);
 };
 
-// Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
   if (typeof fromNode === 'object') {
     fromNode = fromNode.value;
